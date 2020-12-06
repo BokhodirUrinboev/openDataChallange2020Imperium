787580475873731f4d3c03878363aecaa567d776
Bokhodir Urinboev <bokhodir.urinboev@gmail.com>
2020-12-06 11:00:20 +0500

    user login register created

--- openDataChallange2020Imperium/viewer/urls.py
+++ openDataChallange2020Imperium/viewer/urls.py
@@ -3,4 +3,8 @@ from . import views
 app_name = "openDataChallange2020Imperium.viewer"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.sign_up_view, name='signup'),
+    path('login/', views.sign_in_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    # path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
--- openDataChallange2020Imperium/viewer/views.py
+++ openDataChallange2020Imperium/viewer/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user
+from django.contrib.auth import login, logout
+from .forms import CreateUserForm
 
 # Create your views here.
+
+
+@unauthenticated_user
+def sign_up_view(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('openDataChallange2020Imperium.viewer:login')
+
+
+    else:
+        form = CreateUserForm()
+    return render(request, 'account/register.html', {'form': form})
+
+
+@unauthenticated_user
+def sign_in_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('openDataChallange2020Imperium.viewer:index')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'account/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('openDataChallange2020Imperium.viewer:index')
 
 
 def index(request):
