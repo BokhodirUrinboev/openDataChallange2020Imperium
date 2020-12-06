7b3f79bd10f78270d665eef62714d6001f416ed0
Bokhodir Urinboev <bokhodir.urinboev@gmail.com>
2020-12-06 22:26:55 +0500

    final code

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,22 @@
 jQuery(function ($) {
-        $(window).scroll(function fix_element() {
-            $('.navbar').css(
-                $(window).scrollTop() > 0
-                    ? { 'background': 'rgba(255,255,255, .7)' }
-                    : { 'background': 'transparent' }
-            );
-            return fix_element;
-        }());
+    $(window).scroll(function fix_element() {
+        $('.navbar').css(
+            $(window).scrollTop() > 0
+                ? {'background': 'rgba(255,255,255, .7)'}
+                : {'background': 'transparent'}
+        );
+        return fix_element;
+    }());
+});
+
+$(document).ready(function () {
+    $('#regions').change(function () {
+        var unselectedValues = $(this).find('option:not(:selected)').map(function () {
+            return this.value;
+        }).get();
+
+        console.log(unselectedValues);
     });
+    $('#submit').on('click', function () {
+    })
+})
